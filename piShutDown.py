058a4076f2002efd9956ea9828a2b7368d779070
KJ-DET <KJ-DET@users.noreply.github.com>
2018-02-04 23:56:29 +0530

    Update piShutDown.py

--- piShutDown.py
+++ piShutDown.py
@@ -5,13 +5,6 @@ ButtonPin = 40
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(ButtonPin,GPIO.IN,pull_up_down=GPIO.PUD_UP)
 
-#try:
-#    while True:
-#if GPIO.input(ButtonPin) == GPIO.LOW:
 GPIO.wait_for_edge(ButtonPin,GPIO.FALLING)
 print("Open, initiate Shutdown")
 subprocess.call(['shutdown' , '-h', 'now'], shell=False)
-#else:
-#    print("Closed")
-#except KeyboardInterrupt:
-#    GPIO.cleanup()
